b0562309d782240720d13fccd4c3abb668700533
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-29 13:22:01 +0200

    add /dev vite playground

---
+++ dev/App.tsx
@@ -0,0 +1,30 @@
+import type { Component } from "solid-js";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import { Hello } from "../src";
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <h1>
+          <Hello></Hello>
+        </h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
