95a75b9e52b3fa6630d669f4dd3f44e0a852d31a
Damian Tarnawski <gthetarnav@gmail.com>
2023-01-16 14:08:01 +0100

    Replace custom tsup config with tsup-preset-solid

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,52 +1,13 @@
-import { defineConfig, Options } from 'tsup'
-import { solidPlugin } from 'esbuild-plugin-solid'
+import { defineConfig } from 'tsup-preset-solid'
 
-type CustomEntry = { readonly dev: boolean; readonly solid: boolean }
-
-export default defineConfig(config => {
-  const watching = !!config.watch
-  const inputFilename = 'src/index.tsx'
-
-  const packageEntries: CustomEntry[] = [
-    { dev: false, solid: false },
-    { dev: true, solid: false },
-    { dev: false, solid: true },
-    { dev: true, solid: true },
-  ]
-
-  return packageEntries.map(({ dev, solid }, i) => {
-    const outFilename = `index${dev ? '.dev' : ''}${solid ? '.solid' : ''}`
-
-    return {
-      watch: watching,
-      target: 'esnext',
-      format: watching || solid ? 'esm' : ['cjs', 'esm'],
-      clean: i === 0,
-      dts: i === 0 ? inputFilename : undefined,
-      entry: { [outFilename]: inputFilename },
-      treeshake: watching ? undefined : { preset: 'safest' },
-      replaceNodeEnv: true,
-      esbuildOptions(options) {
-        options.define = {
-          ...options.define,
-          'process.env.NODE_ENV': dev ? `"development"` : `"production"`,
-          'process.env.PROD': dev ? 'false' : 'true',
-          'process.env.DEV': dev ? 'true' : 'false',
-          'import.meta.env.NODE_ENV': dev ? `"development"` : `"production"`,
-          'import.meta.env.PROD': dev ? 'false' : 'true',
-          'import.meta.env.DEV': dev ? 'true' : 'false',
-        }
-        options.jsx = 'preserve'
-
-        if (!dev) options.drop = ['console', 'debugger']
-
-        return options
-      },
-      outExtension: ({ format }) => {
-        if (format === 'esm' && solid) return { js: '.jsx' }
-        return {}
-      },
-      esbuildPlugins: !solid ? [solidPlugin() as any] : undefined,
-    } satisfies Options
-  })
-})
+export default defineConfig(
+  {
+    entry: 'src/index.tsx',
+    devEntry: true,
+    dropConsole: true,
+  },
+  {
+    // Enable this to write export conditions to package.json
+    // writePackageJson: true,
+  },
+)
