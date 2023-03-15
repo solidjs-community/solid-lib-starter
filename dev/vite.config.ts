ba32c890b2e643178c51b85785f191e6ce479d08
bigmistqke <vandijckv@gmail.com>
2023-03-15 20:09:12 +0100

    set a path-alias to the vite.config.ts

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, '../src'),
+    },
+  },
   plugins: [
     solidPlugin(),
     {
